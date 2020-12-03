13969e7bee9fe6bb8aab183fd9b368b0a8030d7e
Dmitriy Lobanov <alienuruchie@gmail.com>
2020-12-03 14:44:05 +0300

    HM14_Task1_Added

--- src/Homework14/Task1/Runner.java
+++ src/Homework14/Task1/Runner.java
@@ -2,6 +2,7 @@ package Homework14.Task1;
 
 import Homework13.Task2_3.*;
 
+import java.util.Comparator;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -19,16 +20,18 @@ public class Runner {
     public static void main(String[] args) {
 
         Map<Pet, Doctor> petsMap = new TreeMap<>();
-        petsMap.put(new Parrot("Mr.Anderson", "just cat", 2, false), new Doctor("Dr.Acula", 666));
+        petsMap.put(new Parrot("Kilmister", "just cat", 2, false), new Doctor("Dr.Acula", 666));
         petsMap.put(new Parrot("Kilmister", "Jack Daniels", 3, true), new Doctor("Dr.Alibi", 7));
         petsMap.put(new Parrot("Cotton's Parrot", "Ara yellow-blue", 33, true), new Doctor("Dr.Strange", 13));
 
         System.out.println(petsMap);
 
-        Map<Pet, Doctor> petsMap2 = new TreeMap<>();
-        petsMap2.put(new Parrot("Mr.Anderson", "just cat", 2, false), new Doctor("Dr.Acula", 666));
-        petsMap2.put(new Parrot("Mr.Anderson", "just cat", 2, false), new Doctor("Dr.Acula", 666));
-        petsMap2.put(new Parrot("Mr.Anderson", "just cat", 2, false), new Doctor("Dr.Acula", 666));
+        ParrotNameComparator comparator = new ParrotNameComparator();
+
+        Map<Parrot, Doctor> petsMap2 = new TreeMap<>(comparator);
+        petsMap2.put(new Parrot("Mr.Anderson", "Jack Daniels", 2, false), new Doctor("Dr.Acula", 666));
+        petsMap2.put(new Parrot("Mr. Meeseeks", "just cat", 2, false), new Doctor("Dr.Acula", 666));
+        petsMap2.put(new Parrot("Mr. Meeseeks", "Ara yellow-blue", 2, false), new Doctor("Dr.Acula", 666));
 
         System.out.println(petsMap2);
 
