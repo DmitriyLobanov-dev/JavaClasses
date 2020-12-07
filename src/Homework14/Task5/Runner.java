c6325682a9c0862b50a82348125827b58a116ab3
Dmitriy Lobanov <alienuruchie@gmail.com>
2020-12-07 10:39:19 +0300

    HW 14 task 5 added

---
+++ src/Homework14/Task5/Runner.java
@@ -0,0 +1,30 @@
+package Homework14.Task5;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Дана упорядоченная последовательность чисел от 1 до N.
+ * Из нее удалили одно число, а оставшиеся перемешали.
+ * Найти удаленное число за 1 проход по массиву (т.е. ыцикл
+ * for используется только 1 раз).
+ */
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+//        List<Integer> list = List.of(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
+//        Integer[] array = (Integer[]) list.toArray();
+
+
+
+        Set<Integer> values = new TreeSet<>();
+        for (Integer el : values) {
+
+        }
+    }
+
+}
