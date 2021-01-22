44e540b7a16fed1a4a73894448830a4ed4c129cf
Dmitriy Lobanov <alienuruchie@gmail.com>
2021-01-22 14:23:02 +0300

    hw19. initiated

--- src/homework16/task2/Runner.java
+++ src/homework16/task2/Runner.java
@@ -3,8 +3,7 @@ package homework16.task2;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 /**
@@ -29,51 +28,25 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        //блок для чтения текста из файла закоментирован для простоты проверки
-//        StringBuilder fileData = new StringBuilder();
-//        try (FileReader fileReader = new FileReader("D:/JavaClasses/src/homework16/task2/task.txt")) {
-//            Scanner scanner = new Scanner(fileReader);
-//            while (scanner.hasNextLine()) {
-//                fileData.append(scanner.nextLine()).append("\n");
-//            }
-//        } catch (FileNotFoundException e) {
-//            System.out.println("Unable to find the file. Please, check if the file exists");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        System.out.println(fileData.toString());
-//        String text = fileData.toString().replaceAll("\\p{Punct}", "").replaceAll("\n", " ").toLowerCase();
+        final String regex = "regex";
+        final Pattern pattern = Pattern.compile(regex);
+        StringBuilder fileData = new StringBuilder();
 
-        String text = "Once upon a time a Wolf was lapping at a spring on a hillside, when, looking\n" +
-                "up, what should he see but a Lamb just beginning to drink a little lower down.";
-        System.out.println(text);
-        text = text.replaceAll("\\p{Punct}", "").replaceAll("\n", " ").toLowerCase();
-
-        List<String> listAllWords = new ArrayList<>(Arrays.asList(text.split(" ")));
-        List<String> listWords = new ArrayList<>();
-        Collections.sort(listAllWords);
-
-        listWords.add(listAllWords.get(0));
-        for (int i = 1; i < listAllWords.size(); i++) {
-            if (!listAllWords.get(i).equals(listAllWords.get(i - 1))) {
-                listWords.add(listAllWords.get(i));
+        try (FileReader fileReader = new FileReader("D:/Self/Java/GitRepo/JavaClasses/src/homework16/task2/task.txt")) {
+            Scanner scanner = new Scanner(fileReader);
+            while (scanner.hasNextLine()) {
+                fileData.append(scanner.nextLine()).append("\n");
             }
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to find the file. Please, check if the file exists");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        String firstLetter = "";
-        for (int i = 0; i < listWords.size(); i++) {
-            if (i == 0) {
-                firstLetter = listWords.get(i).substring(0, 1).toUpperCase();
-                System.out.print(firstLetter + ": " + listWords.get(i));
-            } else {
-                if (firstLetter.equals(listWords.get(i).substring(0, 1).toUpperCase())) {
-                    System.out.print(" " + listWords.get(i));
-                } else {
-                    firstLetter = listWords.get(i).substring(0, 1).toUpperCase();
-                    System.out.print("\n" + firstLetter + ": " + listWords.get(i));
-                }
-            }
-        }
+        System.out.println(fileData.toString());
+
+
+
     }
+
 }
---
+++ src/homework19/task1/CharIncTread.java
@@ -0,0 +1,12 @@
+package homework19.task1;
+
+public class CharIncTread extends Thread{
+
+    public CharIncTread(StringBuilder srt) {
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+}
